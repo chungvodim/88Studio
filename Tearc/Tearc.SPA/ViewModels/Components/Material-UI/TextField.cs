40054a853478ace7350335c2abcfe3d86953a8a2
chungvodim <ba198647@gmail.com>
2017-04-10 22:08:13 +0700

    first step

---
+++ Tearc/Tearc.SPA/ViewModels/Components/Material-UI/TextField.cs
@@ -0,0 +1,9 @@
+﻿namespace ViewModels.Components.MaterialUI
+{
+   public class TextField
+   {
+      public string floatingLabelText { get; set; }
+
+      public string hintText { get; set; }
+   }
+}
