62f7717cba2af3766d3a9fcf0207d70bfdb6ca6b
chungvodim <ba198647@gmail.com>
2017-04-13 22:39:27 +0700

    Localization

---
+++ Tearc/Tearc.SPA/Resources/GlobalResource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tearc.SPA.Resources
+{
+    public class GlobalResource
+    {
+    }
+}
---
+++ Tearc/Tearc.SPA/Resources/GridViewResource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tearc.SPA.Resources
+{
+    public class GridViewResource
+    {
+        public static object ReportToNotFound { get; internal set; }
+    }
+}
